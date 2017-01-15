0a2483810b78dddef3aef03873fdfbb6cf954d4c
André Gonçalves <andregoncalves@tekzenit.com>
2017-01-15 21:54:36 +0000

    Allow the loop to end early

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
 'use strict';
 const pMap = require('p-map');
 
-module.exports = (count, mapper, opts) => pMap(Array(count).fill(), (el, i) => mapper(i), opts);
+const symbolEnd = Symbol('pTimes.end');
+
+module.exports = (count, mapper, opts) => {
+	const results = [];
+	let times = 0;
+
+	for (let i = 0; i < count; i++) {
+		results.push(mapper(i));
+	}
+
+	return new Promise(resolve => {
+		Promise.all(results).then(values => {
+			for (const value of values) {
+				if (value === symbolEnd) {
+					break;
+				}
+
+				times++;
+			}
+
+			resolve(pMap(Array(times).fill(), (el, i) => mapper(i), opts));
+		});
+	});
+};
+
+module.exports.end = symbolEnd;
