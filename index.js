cdb65a5b873a6fdf7d85638a189620caeb358d29
Dimitri Benin <dimitri.benin@gmail.com>
2019-04-02 22:30:38 +0200

    Refactor TypeScript definition to CommonJS compatible export

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ const pTimes = async (count, mapper, options) =>
 	pMap(new Array(count).fill(), (element, index) => mapper(index), options);
 
 module.exports = pTimes;
+// TODO: Remove this for the next major release
 module.exports.default = pTimes;
